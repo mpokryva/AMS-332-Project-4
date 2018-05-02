a2c838f4bbc5f733dd70beff9d9b093ba2307929
mpokryva <mpokryva@gmail.com>
2018-05-02 11:46:03 -0400

    Added images and changed some random things for part 2 (hopefully didn't break).

--- Ex2.m
+++ Ex2.m
@@ -35,38 +35,39 @@ hold off
 
 %%%%%%%%% Part 2 %%%%%%%%%%%%%
 dt = 0.1 * 10 .^ -3; % ms
-tau_f = 10 * 10 .^ -3 % ms
+tau_f = 10 * 10 .^ -3; % ms
 tau = 10 * 10 .^ -3; % ms
-t = 500 * 10 .^ -3 % ms
-f_0 = [5, 55, 75, 130];
-J = [0.1, 0.185, 0.3, 0.4];
-%f_0 = [25];
+t = 500 * 10 .^ -3; % ms
+f_0 = [55, 75, 130];
+%J = [0.1, 0.185, 0.3, 0.4];
 time = zeros(1, t/dt);
 f = zeros(1, t/dt);
+resp = zeros(1, t/dt);
 for k = 1 : length(J)
     for i = 1 : length(f_0)
         figure(i+1);
         hold on;
         f(1) = f_0(i);
+        resp(1) = f_0(i);
         time(1) = 0;
         for j = 1 : t/dt
             mv = mu_v(J(k), N, f(j), tau);
             sv = sigma_v(J(k), N, f(j), tau); 
-            resp = response(mv, sv, v_spk, f_max, N);
-            df = (((f(j) * -1) + resp) / tau_f) * dt;
+            resp(j+1) = response(mv, sv, v_spk, f_max, N);
+            df = (((f(j) * -1) + resp(j+1)) / tau_f) * dt;
             f(j+1) = f(j) + df;
             time(j+1) = time(j) + dt;
         end
-        plot(time, f);
+        plot(time, f, time, resp);
         xlabel("Time (s)")
         ylabel("Firing rate (spk/s)")
         title("Firing rate with J = " + J(k) + ", f_0 = " + f_0(i));
     end 
 end
 
-%legend_labels = strings(1, length(f_0));
+%legend_labels = strings(1, length(J));
 %for i = 1 : length(legend_labels)
- %  legend_labels(i) = strcat("f_0=", num2str(f_0(i)));
+ %   legend_labels(i) = strcat("J=", num2str(J(i)));
 %end
 %lgd = legend(legend_labels);
 %lgd.FontWeight = "bold";
